a9a2fc1a00c57dd7fac0f79fa778223993880f05
Loic Doubinine <l.doubinine@meetic-corp.com>
2013-05-27 15:51:15 +0200

    add masters

--- web/index.php
+++ web/index.php
@@ -49,4 +49,39 @@ $app->get(
         );
     }
 );
+
+$app->get(
+    '/debug',
+    function () use ($app, $filter) {
+        $dao = new \Ztec\BamDash\Dao\BambooData("http://ci-tools/bamboo/rest/api/latest/");
+        $plans = $dao->getAllPlans();
+
+
+        $allPlans = array();
+        foreach ($plans->plans->plan as $plan) {
+            $branches = $dao->getBranches($plan->key);
+            $builds = $dao->getBuilds($plan->key);
+            $status = $dao->getBranchStatus($plan->key);
+            $plan->builds = $builds->results->result;
+            $plan->status = $status;
+            $plan->name = $plan->name. ' master' ;
+            $plan->shortName = 'master' ;
+            $allPlans[] = $plan;
+            foreach ($branches->branches->branch as $branch) {
+                if (preg_match('/' . $filter . '/', $branch->name)) {
+                    $builds = $dao->getBuilds($branch->key);
+                    $status = $dao->getBranchStatus($branch->key);
+                    $branch->builds = $builds->results->result;
+                    $branch->status = $status;
+                    $allPlans[] = $branch;
+                }
+            }
+        }
+        $date = new \DateTime();
+        //print_r($allPlans);
+        //print_r($allPlans);
+        return print_r($allPlans,true);
+    }
+);
+
 $app->run();
